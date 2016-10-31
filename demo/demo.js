0198eb17c185d60c9301b6cb36ada210a052a686
Yoshimura Yuu <m@yyu.pw>
2016-11-01 02:33:49 +0900

    fix to join the room

--- demo/demo.js
+++ demo/demo.js
@@ -4,9 +4,8 @@ const mentaljs = constructor("enxnz2an1739dx6r", true);
 const key = location.hash.slice(1);
 if (key === "") {
     console.log("the key is empty");
-    const dataStream = mentaljs.makeRoom();
-
-    dataStream.log();
+    const messageRoomStream = mentaljs.makeRoom();
+    messageRoomStream.log();
 } else {
     console.log(key);
     mentaljs.joinRoom(key).log();
